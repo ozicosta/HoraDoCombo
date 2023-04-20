696c8c4147ae450194ca72fc878320dbb808564a
Ozieni Costa <61131441+ozicosta@users.noreply.github.com>
2023-04-19 23:47:56 -0300

    Create Sobremesa.java

---
+++ Sobremesa.java
@@ -0,0 +1,18 @@
+public  class  Sobremesa {
+
+     string  privada nome ;
+     preço duplo  privado ;
+
+    public  Sobremesa ( String  nome , double  preco ) {
+        isso . nome = nome ;
+        isso . preço = preço ;
+    }
+
+    public  String  getNome () {
+         nome de retorno ;
+    }
+
+    public  double  getPreco () {
+        retorno  rápido ;
+    }
+}
