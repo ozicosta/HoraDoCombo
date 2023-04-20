ae341d2969b924322c65e783910a25725ad22956
Ozieni Costa <61131441+ozicosta@users.noreply.github.com>
2023-04-19 23:45:34 -0300

    Create Bebida.java

---
+++ Bebida.java
@@ -0,0 +1,17 @@
+public  class  Bebidas {
+     string  privada nome ;
+     preço duplo  privado ;
+
+    public  Bebida ( String  nome , preço duplo  ) {
+        isso . nome = nome ;
+        isso . preço = preço ;
+    }
+
+    public  String  getNome () {
+         nome de retorno ;
+    }
+
+    public  double  getPreco () {
+        retorno  rápido ;
+    }
+}
