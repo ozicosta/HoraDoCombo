57d36f0a1ba187e794fa2d4101e2fa8b4d7b4db2
Ozieni Costa <61131441+ozicosta@users.noreply.github.com>
2023-04-19 23:44:40 -0300

    Create Buguer.java

---
+++ Buguer.java
@@ -0,0 +1,17 @@
+public  class  Hambúrguer {
+     string  privada nome ;
+     preço duplo  privado ;
+
+    public  Burguer ( String  nome , preço duplo  ) {
+        isso . nome = nome ;
+        isso . preço = preço ;
+    }
+
+    public  String  getNome () {
+         nome de retorno ;
+    }
+
+    public  double  getPreco () {
+        retorno  rápido ;
+    }
+}
